60f4f804f582da909d2cc6f0d453a5bad1616b8f
hiruto101 <hiruto101@gmail.com>
2024-06-25 11:35:47 +0800

    ver 2

--- network/models.py
+++ network/models.py
@@ -4,3 +4,20 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
+    content = models.CharField(max_length= 300)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"User: {self.author} Post: {self.content} Date: {self.date.strftime('%d %b %Y %H:%M:%S')}"
+    
+    
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post")
+    
+    class Meta:
+        unique_together = ('user', 'post')
--- network/views.py
+++ network/views.py
@@ -3,12 +3,68 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.core.paginator import Paginator
 
-from .models import User
+from .models import User, Post
 
 
 def index(request):
-    return render(request, "network/index.html")
+    post = Post.objects.all().order_by('-date')
+    paginator = Paginator(post, 10)
+    
+    # create a list of pages
+    num_pages = [x for x in range(1, paginator.count + 1)]
+    
+    page_number = request.GET.get('page')
+    posts = paginator.get_page(page_number)
+    #print(f"DEBUG", num_pages)
+    return render(request, "network/index.html", {
+        "posts":posts,
+        "pages": num_pages
+    })
+
+
+def profile(request):
+    posts = Post.objects.filter(author = request.user).order_by('-date')
+    paginator = Paginator(posts, 10)
+    print(f"Debug2", paginator.num_pages)
+    # create a list of pages
+    if(paginator.num_pages <= 1):
+        num_pages = [1]
+    else:
+        num_pages = [x for x in range(1, paginator.count + 1)]
+    
+    page_number = request.GET.get('page')
+    posts = paginator.get_page(page_number)
+    
+    return render(request, "network/profile.html", {
+        "posts":posts,
+        "pages": num_pages
+    })
+
+
+def post(request):
+    content = request.POST.get("content")
+       
+    try: 
+        # Create a new Post object
+        post = Post()
+        post.author = request.user
+        post.content = content
+        
+        # Save the Post
+        post.save()
+        
+        print(f"author: {post.author} and content: {post.content}")
+        
+        return HttpResponseRedirect(reverse("index"))
+    except Exception as error:
+        print(f"Error {error}")
+        return HttpResponseRedirect(reverse("index"))
+    
+
+def edit(request):
+    pass
 
 
 def login_view(request):
@@ -61,3 +117,4 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
